b0c1d899a2734ef44b83fc10c8b37f2e4e130cd7
Josh Callender <jcallender@change.org>
2015-01-06 10:30:57 -0800

    created a forEach helper that will add things like indx, total, isFirst element, isLast element, and iterate over objects, collections, or arrays

--- shared/helpers.js
+++ shared/helpers.js
@@ -108,6 +108,59 @@ module.exports = function(Handlebars, getTemplate) {
 
       // Call the original helper with new context.
       return oldEach.call(this, context, options);
+    },
+
+    /**
+     * Create a `forEach` helper that works on a few more cases and gives more flexibility
+     * when dealing with arrays, objects, or collections
+     */
+    forEach: function(collection, opts) {
+      var len = collection && collection.length,
+          app = this._app || this.app,
+          isCollection = app.modelUtils.isCollection(collection),
+          buffer = '';
+
+      if (_.isEmpty(collection)) {
+        return opts.inverse(_.extend({}, this, {
+          _app: app,
+          _model: this._model || this.model,
+          _collection: this._collection || this.collection,
+          _view: this._view || this.view
+        }));
+      }
+
+      // iterate the models on a collection
+      if (isCollection) {
+        collection = collection.models
+      }
+
+      _.each(collection, function (value, key) {
+        if (isCollection && opts.hash.toJSON) {
+          value = value.toJSON();
+        }
+
+        var item = _.extend({
+          key: key,
+          value: value,
+          _app: this._app || this.app,
+          _model: this._model || this.model,
+          _collection: this._collection || this.collection,
+          _view: this._view || this.view
+        }, opts.hash);
+
+        // adding extra attributes to an item for array traversal
+        if (_.isArray(collection) || isCollection) {
+          item = _.extend(item, {
+            _total: len,
+            _isFirst: key === 0,
+            _isLast: key === (len - 1)
+          });
+        }
+
+        buffer += opts.fn(item);
+      }.bind(this));
+
+      return buffer;
     }
   };
 };
