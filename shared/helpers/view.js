8ed0aa878954548bbd7d7d5b6d7dca1ccaa9947c
Josh Callender <jcallender@change.org>
2015-01-07 16:25:43 -0800

    cleaned up the view function

--- shared/helpers/view.js
+++ shared/helpers/view.js
@@ -2,18 +2,17 @@
  * Helper to create new views in the templates
  */
 
-var getProperty = require('../../lib/getProperty');
+var getProperty = require('../../lib/getProperty'),
+    isServer = typeof window === 'undefined',
+    BaseView;
 
 module.exports = function (Handlebars) {
   return function (viewName, options) {
-    var ViewClass, html, viewOptions, view;
+    var ViewClass, html, viewOptions, view, app;
 
-    if (!BaseView) {
-      BaseView = require('rendr/shared/base/view');
-    }
     viewOptions = options.hash || {};
+    app = getProperty('_app', this, options);
 
-    var app = getProperty('_app', this, options);
     // Pass through a reference to the app.
     if (app) {
       viewOptions.app = app;
@@ -23,38 +22,46 @@ module.exports = function (Handlebars) {
     }
 
     if (isServer) {
-      // Pass through a reference to the parent view.
       var parentView = getProperty('_view', this, options);
-      if (parentView) {
-        viewOptions.parentView = parentView;
-      }
-
-      // get the Backbone.View based on viewName
-      ViewClass = BaseView.getView(viewName, app.options.entryPath);
-      view = new ViewClass(viewOptions);
-
-      // create the outerHTML using className, tagName
-      html = view.getHtml();
-      return new Handlebars.SafeString(html);
+      html = getServerHtml(viewName, viewOptions, parentView);
+    } else {
+      html = getClientPlaceholder(viewName, viewOptions);
     }
 
-    // only create an element to pass attributes to a single point for view instantiation for client-side
-
-    // Builds a fetch_summary attribute
-    viewOptions = BaseView.parseModelAndCollection(app.modelUtils, viewOptions);
-    var fetchSummary = BaseView.extractFetchSummary(app.modelUtils, viewOptions);
-    fetchSummary = JSON.stringify(fetchSummary)
-
-    viewOptions['fetch_summary'] = fetchSummary
-    viewOptions = _.omit(viewOptions, ['model', 'collection', 'app'])
-
-    // create a list of data attributes
-    var attrString = _.inject(viewOptions, function(memo, value, key) {
-      return memo += " data-" + key + "=\"" + _.escape(value) + "\"";
-    }, '');
-
-    return new Handlebars.SafeString(
-      '<div data-render="true" ' + attrString +' data-view="'+ viewName +'"></div>'
-    )
+    return new Handlebars.SafeString(html);
   };
 };
+
+function getServerHtml(viewName, viewOptions, parentView) {
+  if (!BaseView) { BaseView = require('rendr/shared/base/view'); }
+
+  // Pass through a reference to the parent view.
+  if (parentView) { viewOptions.parentView = parentView; }
+
+  // get the Backbone.View based on viewName
+  ViewClass = BaseView.getView(viewName, viewOptions.app.options.entryPath);
+  view = new ViewClass(viewOptions);
+
+  // create the outerHTML using className, tagName
+  return view.getHtml();
+}
+
+function getClientPlaceholder(viewName, viewOptions) {
+  if (!BaseView) { BaseView = require('rendr/shared/base/view'); }
+  var fetchSummary;
+
+  // Builds a fetch_summary attribute
+  viewOptions = BaseView.parseModelAndCollection(viewOptions.app.modelUtils, viewOptions);
+  fetchSummary = BaseView.extractFetchSummary(viewOptions.app.modelUtils, viewOptions);
+  fetchSummary = JSON.stringify(fetchSummary)
+
+  viewOptions['fetch_summary'] = fetchSummary
+  viewOptions = _.omit(viewOptions, ['model', 'collection', 'app'])
+
+  // create a list of data attributes
+  var attrString = _.inject(viewOptions, function(memo, value, key) {
+    return memo += " data-" + key + "=\"" + _.escape(value) + "\"";
+  }, '');
+
+  return '<div data-render="true" ' + attrString +' data-view="'+ viewName +'"></div>';
+}
