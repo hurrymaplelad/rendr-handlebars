7631e80ba44854f431bf4c7e08d9716cf4901c58
Josh Callender <jcallender@change.org>
2015-02-24 16:44:58 -0800

    verify that the string is what we'd want and the data is parsed the way we expect

--- test/shared/helpers/serverToClientJson.test.js
+++ test/shared/helpers/serverToClientJson.test.js
@@ -8,7 +8,15 @@ describe('serverToClientJson', function () {
   var data = { key: 'права' }
 
   it('should add JSON.parse and unescape to the string', function () {
+    var result = subject(data),
+        expectedResult = 'JSON.parse(unescape("%7B%22key%22%3A%22%u043F%u0440%u0430%u0432%u0430%22%7D"))';
+
+    expect(expectedResult).to.equal(result.string);
+  });
+
+  it('should result in the same data after eval', function () {
     var result = subject(data);
     expect(eval(result.string)).to.deep.equal(data);
   });
+
 });
